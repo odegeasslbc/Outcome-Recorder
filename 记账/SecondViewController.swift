60b28d082d2744751898896f4c971e0d8cef89c3
codegeasslbc <534060553@qq.com>
2015-06-19 18:23:59 -0400

    Initial Commit

---
+++ 记账/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  记账
+//
+//  Created by 刘炳辰 on 15/6/19.
+//  Copyright (c) 2015年 刘炳辰. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
